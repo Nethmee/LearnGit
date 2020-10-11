a5e434fe1d3177a7d978d1aec9751a2582aa3db4
Nethmee <nssellahewa@gmail.com>
2020-10-11 19:50:02 +0530

    commit 3 develop

--- Develop/develop.js
+++ Develop/develop.js
@@ -1,2 +1,3 @@
 console.log("Im about to learn git rebase")
 console.log("set the upstream branch of the develop branch")
+console,log("commit 1 after the rebase-feature-1 branch was created and made 2 commits to it.");
